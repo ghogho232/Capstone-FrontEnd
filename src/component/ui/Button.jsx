d0ffda4098bb546f457c6da542623d09c236349b
zzxew <163097720+zzxew@users.noreply.github.com>
2024-05-02 03:50:47 +0900

    Delete src directory

--- src/component/ui/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Button(props){
-    const{title, onClick, id, className, style} = props;
-    return (
-        <button className={className} id={id} onClick={onClick} style={style}>{title}</button>
-      );
-}
-export default Button;
